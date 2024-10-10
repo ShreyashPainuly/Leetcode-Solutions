2a9be0f02a0221fd22a6c9c8f2ebaa32ae63a09b
Shreyash Painuly <shreyashpainulyog@gmail.com>
2024-10-10 21:46:41 +0530

    initial commit

--- 509. Fibonacci Number/Solution.cpp
+++ 509. Fibonacci Number/Solution.cpp
@@ -1,3 +1,8 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
 // Example 1:
 // Input: n = 2
 // Output: 1
@@ -12,6 +17,10 @@
 // Input: n = 4
 // Output: 3
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+// Recursion Approach:
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
 class Solution {
 public:
