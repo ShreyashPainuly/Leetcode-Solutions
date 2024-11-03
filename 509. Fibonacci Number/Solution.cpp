18fd7064ae438288757b10244e734cb13a251eff
Shreyash Painuly <shreyashpainulyog@gmail.com>
2024-11-03 14:59:32 +0530

    initial commit

--- 509. Fibonacci Number/Solution.cpp
+++ 509. Fibonacci Number/Solution.cpp
@@ -19,7 +19,7 @@
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 
 // Recursion Approach:
-// Time Complexity: O(n)
+// Time Complexity: O(2^n)
 // Space Complexity: O(1)
 
 class Solution {
@@ -33,3 +33,84 @@ public:
         }
     }
 };
+
+// Recursion and Memoization Approach:
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        if(n <= 1) {
+            return n;
+        }
+
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    }
+    int fib(int n) {
+
+        //Base Case
+        if(n <= 1) {
+            return n;
+        }
+
+        vector<int> dp(n+1, -1);
+
+        return solve(n, dp);
+    }
+};
+
+// DP Bottom - up Approach:
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    int fib(int n) {
+        //Base Case
+        if(n <= 1) {
+            return n;
+        }
+
+        vector<int> dp(n+1, -1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
+
+// Constant Space Approach:
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int fib(int n) {
+        //Base Case
+        if(n <= 1) {
+            return n;
+        }
+
+        int a = 0;
+        int b = 1;
+        int c;
+
+        for(int i = 1; i<n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
+
